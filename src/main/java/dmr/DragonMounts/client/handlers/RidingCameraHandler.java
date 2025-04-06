8ac2ef8de4f01ac29ab05b08ecdcda5df2a6bf7d
HH <14839691+Horeak@users.noreply.github.com>
2025-04-06 18:58:03 +0200

    fix: Fixed the riding camera offset not working as intended

--- src/main/java/dmr/DragonMounts/client/handlers/RidingCameraHandler.java
+++ src/main/java/dmr/DragonMounts/client/handlers/RidingCameraHandler.java
@@ -2,14 +2,13 @@ package dmr.DragonMounts.client.handlers;
 
 import dmr.DragonMounts.config.ClientConfig;
 import dmr.DragonMounts.server.entity.DMRDragonEntity;
-import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.util.Mth;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.client.event.ViewportEvent.ComputeCameraAngles;
+import net.neoforged.neoforge.client.event.CalculateDetachedCameraDistanceEvent;
 
 @EventBusSubscriber(Dist.CLIENT)
 public class RidingCameraHandler {
@@ -17,23 +16,19 @@ public class RidingCameraHandler {
 	private static float lastCameraIncrease;
 
 	@SubscribeEvent
-	public static void flightCamera(ComputeCameraAngles setup) {
+	public static void flightCamera(CalculateDetachedCameraDistanceEvent event) {
 		LocalPlayer currentPlayer = Minecraft.getInstance().player;
-		Camera info = setup.getCamera();
 
 		if (currentPlayer != null && currentPlayer.getRootVehicle() instanceof DMRDragonEntity) {
-			if (setup.getCamera().isDetached()) {
-				float maxZoom = info.getMaxZoom(ClientConfig.RIDING_CAMERA_OFFSET.get());
-				float gradualIncrease = Mth.lerp(0.25f, lastCameraIncrease, maxZoom);
-				info.move(gradualIncrease * -1f, (gradualIncrease / 2f) * -1f, 0f);
-				lastCameraIncrease = gradualIncrease;
-				return;
-			}
+			float gradualIncrease = Mth.lerp(0.02f, lastCameraIncrease, ClientConfig.RIDING_CAMERA_OFFSET.get());
+			event.setDistance(gradualIncrease);
+			lastCameraIncrease = gradualIncrease;
+			return;
 		}
 
 		if (lastCameraIncrease > 0) {
-			lastCameraIncrease = Mth.lerp(0.25f, lastCameraIncrease, 0);
-			info.move(0, lastCameraIncrease, 0);
+			lastCameraIncrease = Mth.lerp(0.25f, lastCameraIncrease, 4f); //4f is the default camera distance set in @net.minecraft.client.Camera#setup:68
+			event.setDistance(lastCameraIncrease);
 		}
 	}
 }
