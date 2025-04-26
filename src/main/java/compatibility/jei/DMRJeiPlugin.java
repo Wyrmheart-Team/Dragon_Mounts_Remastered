97d4378fd530b6a70972e68671d70b64f9895575
HH <14839691+Horeak@users.noreply.github.com>
2025-04-26 21:28:47 +0200

    Update DMRJeiPlugin.java

--- src/main/java/compatibility/jei/DMRJeiPlugin.java
+++ src/main/java/compatibility/jei/DMRJeiPlugin.java
@@ -4,7 +4,6 @@ import dmr.DragonMounts.DMR;
 import dmr.DragonMounts.registry.DragonArmorRegistry;
 import dmr.DragonMounts.registry.ModComponents;
 import dmr.DragonMounts.registry.ModItems;
-import dmr.DragonMounts.server.recipes.NetheriteArmorRecipe;
 import dmr.DragonMounts.types.armor.DragonArmor;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
@@ -24,12 +23,12 @@ import java.util.List;
 
 @JeiPlugin
 public class DMRJeiPlugin implements IModPlugin {
-
+	
 	@Override
 	public ResourceLocation getPluginUid() {
 		return DMR.id("jei_plugin");
 	}
-
+	
 	@Override
 	public void registerItemSubtypes(ISubtypeRegistration registration) {
 		registration.registerSubtypeInterpreter(ModItems.DRAGON_SPAWN_EGG.get(), new DragonSpawnEggInterpreter());
@@ -40,19 +39,33 @@ public class DMRJeiPlugin implements IModPlugin {
 	
 	@Override
 	public void registerRecipes(IRecipeRegistration registration) {
-		List<RecipeHolder<? extends CustomRecipe>> recipes = new ArrayList<>();
-		var armorRecipe = new RecipeHolder<>(DMR.id("netherite_armor_recipe"), new NetheriteArmorRecipe());
-		recipes.add(armorRecipe);
-		
+		List<RecipeHolder<CraftingRecipe>> recipes = new ArrayList<>();
+		addNetheriteArmorRecipe(recipes);
+		addBlankDragonEggRecipe(recipes);
+		registration.addRecipes(RecipeTypes.CRAFTING, recipes);
+	}
+	
+	public static void addNetheriteArmorRecipe(List<RecipeHolder<CraftingRecipe>> recipes) {
 		var result = new ItemStack(ModItems.DRAGON_ARMOR.get());
 		var diamondArmor = new ItemStack(ModItems.DRAGON_ARMOR.get());
 		DragonArmor.setArmorType(diamondArmor, DragonArmorRegistry.getDragonArmor("diamond"));
 		DragonArmor.setArmorType(result, DragonArmorRegistry.getDragonArmor("netherite"));
 		
-		
 		var netheriteArmorRecipe = new ShapelessRecipe("dragon", CraftingBookCategory.MISC, result, NonNullList.of(Ingredient.EMPTY, Ingredient.of(diamondArmor), Ingredient.of(Items.NETHERITE_INGOT)));
 		var netheriteArmorRecipeHolder = new RecipeHolder<CraftingRecipe>(DMR.id("netherite_armor_recipe"), netheriteArmorRecipe);
-		registration.addRecipes(RecipeTypes.CRAFTING, List.of(netheriteArmorRecipeHolder));
+		recipes.add(netheriteArmorRecipeHolder);
+	}
+	
+	public static void addBlankDragonEggRecipe(List<RecipeHolder<CraftingRecipe>> recipes) {
+		var result = new ItemStack(ModItems.BLANK_EGG_BLOCK_ITEM.get());
+		var eggRecipe = new ShapelessRecipe("dragon", CraftingBookCategory.MISC, result, NonNullList.of(Ingredient.EMPTY, Ingredient.of(ModItems.DRAGON_EGG_BLOCK_ITEM.get())));
+		var eggRecipeHolder = new RecipeHolder<CraftingRecipe>(DMR.id("blank_egg_recipe"), eggRecipe);
+		recipes.add(eggRecipeHolder);
+	}
+	
+	public static void addDragonEggRecipe(List<RecipeHolder<CraftingRecipe>> recipes) {
+		var result = new ItemStack(ModItems.DRAGON_EGG_BLOCK_ITEM.get());
+		var eggRecipe = new ShapelessRecipe("dragon", CraftingBookCategory.MISC, result, NonNullList.of(Ingredient.EMPTY, Ingredient.of(ModItems.DRAGON_EGG_BLOCK_ITEM.get()), Ingredient.of(Items.EGG)));
 	}
 }
 
