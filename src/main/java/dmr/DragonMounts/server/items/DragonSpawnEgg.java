a08b9769bdf0370e20a8f95a2dc085292835be48
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-09-06 13:29:51 +0100

    fix: Dragons spawned from an egg have their parent's breed.

--- src/main/java/dmr/DragonMounts/server/items/DragonSpawnEgg.java
+++ src/main/java/dmr/DragonMounts/server/items/DragonSpawnEgg.java
@@ -1,20 +1,28 @@
 package dmr.DragonMounts.server.items;
 
+import java.util.Optional;
+
 import dmr.DragonMounts.ModConstants;
 import dmr.DragonMounts.ModConstants.NBTConstants;
 import dmr.DragonMounts.registry.DragonBreedsRegistry;
 import dmr.DragonMounts.registry.ModComponents;
 import dmr.DragonMounts.registry.ModEntities;
 import dmr.DragonMounts.registry.ModItems;
+import dmr.DragonMounts.server.entity.TameableDragonEntity;
 import dmr.DragonMounts.types.dragonBreeds.DragonHybridBreed;
 import dmr.DragonMounts.types.dragonBreeds.IDragonBreed;
 import dmr.DragonMounts.types.dragonBreeds.IDragonBreed.Variant;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.common.DeferredSpawnEggItem;
 
 public class DragonSpawnEgg extends DeferredSpawnEggItem {
@@ -72,6 +80,33 @@ public class DragonSpawnEgg extends DeferredSpawnEggItem {
         stack.set(ModComponents.DRAGON_VARIANT, variant != null ? variant.id() : null);
 
         return stack;
+    }
+
+    @Override
+    public Optional<Mob> spawnOffspringFromSpawnEgg(Player player, Mob p_mob, EntityType<? extends Mob> entityType,
+            ServerLevel serverLevel, Vec3 pos, ItemStack stack) {
+        var mob = super.spawnOffspringFromSpawnEgg(player, p_mob, entityType, serverLevel, pos, stack);
+        if (mob.isEmpty()) {
+          return mob;
+        }
+
+        var parent = (TameableDragonEntity) p_mob;
+        var child = (TameableDragonEntity) mob.get();
+
+            if (stack.has(ModComponents.DRAGON_BREED)) {
+          child.setBreed(DragonBreedsRegistry.getDragonBreed(stack.get(ModComponents.DRAGON_BREED)));
+          
+          if (stack.has(ModComponents.DRAGON_VARIANT)) {
+            child.setVariant(stack.get(ModComponents.DRAGON_VARIANT));
+          } else {
+            var parentVariant = parent.getVariant();
+            child.setVariant(parentVariant != null ? parentVariant.id() : null);
+          }
+            } else {
+          child.setBreed(parent.getBreed());
+        }
+
+        return mob;
     }
 
     @Override
